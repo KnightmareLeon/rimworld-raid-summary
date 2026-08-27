a51c428c9a6892703ea72f6cd62291eda26cbdd2
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-08-27 23:39:11 +0800

    build: setup cs project and verify dll loading

---
+++ Source/RaidSummary/RaidSummary.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace RaidSummary
+{
+    public class RaidSummaryMod : Mod
+    {
+        public RaidSummaryMod(ModContentPack content) : base(content)
+        {
+            Log.Message("[Raid Summary] initialized");
+        }
+    }
+}
