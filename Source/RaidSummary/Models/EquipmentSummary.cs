80fe1f87388e4bd4e33c15f375c29aaf024e8789
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-08-30 23:31:59 +0800

    feat: add equipment summary model

---
+++ Source/RaidSummary/Models/EquipmentSummary.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine.PlayerLoop;
+using Verse;
+
+namespace RaidSummary.Models
+{
+    public class EquipmentSummary
+    {
+        public ThingDef EquipmentDef { get; set; }
+        public Dictionary<QualityCategory, int> QualityCounts { get; set; }
+            = new Dictionary<QualityCategory, int>();
+
+        public int GetTotal()
+        {
+            int res = 0;
+            foreach (var value in QualityCounts.Values)
+            {
+                res += value;
+            }
+
+            return res;
+        }
+    }
+}
