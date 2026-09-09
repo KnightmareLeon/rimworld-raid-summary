394a9c39479fa943c0c237f440ed3b18042165fe
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-09-09 21:24:25 +0800

    feat: add new method to add label with icon for things

--- Source/RaidSummary/UI/RaidSummaryListing.cs
+++ Source/RaidSummary/UI/RaidSummaryListing.cs
@@ -20,6 +20,15 @@ namespace RaidSummary.UI
             EndLine();
         }
 
+        public void DrawLabelForThing(ThingDef tDef, ref int indentLevel, string extraInfo = "")
+        {
+            if (tDef.uiIcon != null && tDef.uiIcon != BaseContent.BadTex)
+                indentLevel++;
+				Widgets.DefIcon(new Rect(XAtIndentLevel(indentLevel) - 6f, curY, 20f, 20f), tDef, null, 1f, null, drawPlaceholder: true);
+            LabelLeft(tDef.LabelCap + extraInfo, null, indentLevel);
+            EndLine();
+        }
+
         public void DrawSection(TreeNode node, string label, int indentLevel, int openMask)
         {
             OpenCloseWidget(node, indentLevel, openMask);
