e09ddd2c85d36c63ba41a726e9cdcc10bd10e4b7
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-09-10 11:01:08 +0800

    feat(ui): create custom tree nodes for raid summary

---
+++ Source/RaidSummary/UI/ThingSummaryNode.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace RaidSummary.UI
+{
+    public class ThingSummaryNode : TreeNode
+    {
+        public TreeNode QualitiesNode {get; private set;} = new TreeNode();
+        public TreeNode MaterialsNode {get; private set;} = new TreeNode();
+        public ThingSummaryNode(int openMask)
+        {
+            QualitiesNode.SetOpen(openMask, false);
+            MaterialsNode.SetOpen(openMask, false);
+        }
+
+
+    }
+}
