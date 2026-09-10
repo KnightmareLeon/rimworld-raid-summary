b7257cb08212f5338de96be021c3cbff90c98f06
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-09-10 16:24:57 +0800

    chore: clean up unreferenced legacy methods

--- Source/RaidSummary/UI/RaidSummaryListing.cs
+++ Source/RaidSummary/UI/RaidSummaryListing.cs
@@ -6,15 +6,6 @@ namespace RaidSummary.UI
 {
     public class RaidSummaryListing : Listing_Tree
     {
-        public bool DrawNode(TreeNode node, string label, int indentLevel, int openMask)
-        {
-            bool changed = OpenCloseWidget(node, indentLevel, openMask);
-
-            LabelLeft(label, null, indentLevel);
-
-            return changed;
-        }
-
         public void DrawLabel(string label, int indentLevel)
         {
             LabelLeft(label, null, indentLevel);
