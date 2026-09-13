97d0b774a61b193e175a5acfbb79224d4c51b883
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-09-13 20:11:19 +0800

    refactor: reduce visibility on faction

--- Source/RaidSummary/UI/RaidSummaryWindow.cs
+++ Source/RaidSummary/UI/RaidSummaryWindow.cs
@@ -161,7 +161,7 @@ namespace RaidSummary.UI
 
             Text.Font = GameFont.Medium;
             string windowHeader = summary.IsFactionEnemy() ? "Raid" : "Friendlies";
-            listing.DrawLabel($"{summary.Faction.Name}'s {windowHeader} Summary", indentLevel);
+            listing.DrawLabel($"{summary.GetFactionName()}'s {windowHeader} Summary", indentLevel);
 
             Text.Font = GameFont.Small;
             listing.GapLine();
