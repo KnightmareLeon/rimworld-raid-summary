84612e0598abb125178ff1d35734110715b008f9
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-09-25 23:53:57 +0800

    refactor: create defs namespace

--- Source/RaidSummary/Patches/IncidentWorkerRaidPatch.cs
+++ Source/RaidSummary/Patches/IncidentWorkerRaidPatch.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using HarmonyLib;
 using RaidSummary.Models;
 using RaidSummary.UI;
-using RaidSummary.Utilities;
+using RaidSummary.Defs;
 using RimWorld;
 using Verse;
 
